1205a6e291aed55aeb8d94181bf67c0ab38d098b
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-07-25 09:21:01 +0530

    add solution using stack inside remove outermost paranthesis

--- stack_Queue/remove_Outermost_Parentheses.js
+++ stack_Queue/remove_Outermost_Parentheses.js
@@ -1,22 +1,43 @@
-var removeOuterParentheses = function(s) {
-   let str=""
-    let openingParenthesesCount=0
-    for(let i=0;i<s.length;i++){
-        if(s[i]==="("){
+var removeOuterParentheses = function (s) {
+    let str = ""
+    let openingParenthesesCount = 0
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
             openingParenthesesCount++
-            if(openingParenthesesCount>1){
-            str+=s[i]
+            if (openingParenthesesCount > 1) {
+                str += s[i]
             }
-        }else{
+        } else {
             openingParenthesesCount--
 
-            if(openingParenthesesCount===0){
+            if (openingParenthesesCount === 0) {
                 continue
             }
-            str+=s[i]
+            str += s[i]
         }
 
     }
     return str
 
+};
+
+
+// using stack
+var removeOuterParentheses = function (s) {
+    let stack = []
+    let str = ""
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(s[i])
+            if (stack.length > 1) {
+                str += s[i]
+            }
+        } else {
+            stack.pop()
+            if (stack.length === 0) continue
+            str += s[i]
+        }
+    }
+
+    return str
 };
