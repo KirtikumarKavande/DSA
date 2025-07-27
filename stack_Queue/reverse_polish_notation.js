4c731e76d381863fb56335ceb41e79453a5bd9af
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-07-27 09:42:19 +0530

    reverse polish notation method 2

--- stack_Queue/reverse_polish_notation.js
+++ stack_Queue/reverse_polish_notation.js
@@ -17,3 +17,36 @@ var evalRPN = function(tokens) {
 
     return stack[stack.length-1]  
 };
+
+// method 2
+var evalRPN = function(tokens) {
+    let stack = [];
+    
+    for(let i = 0; i < tokens.length; i++){
+        if(!isNaN(+tokens[i])){
+            stack.push(+tokens[i]);  
+        } else {
+            let firstEle = stack.pop();
+            let secondEle = stack.pop();
+            let result;
+            
+            switch(tokens[i]) {
+                case '+':
+                    result = secondEle + firstEle;
+                    break;
+                case '-':
+                    result = secondEle - firstEle;
+                    break;
+                case '*':
+                    result = secondEle * firstEle;
+                    break;
+                case '/':
+                    result = Math.trunc(secondEle / firstEle);
+                    break;
+            }
+            stack.push(result);
+        }
+    }
+    
+    return stack[0];  
+};
