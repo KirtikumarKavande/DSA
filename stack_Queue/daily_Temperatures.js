6a0e1644fc0ae5c1d34a325702f8a95ac907583c
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-08-05 10:16:08 +0530

    dailyTemperatures function to use monotonic decreasing stack approach 2

--- stack_Queue/daily_Temperatures.js
+++ stack_Queue/daily_Temperatures.js
@@ -1,4 +1,25 @@
-// monotonic decreasing stack
+
+var dailyTemperatures = function(temperatures) {
+    let monotonicDecreasingStack = []
+    let result = new Array(temperatures.length).fill(0)
+    
+    for(let i = temperatures.length-1; i >= 0; i--){
+        
+        while(monotonicDecreasingStack.length > 0 && 
+              temperatures[i] >= temperatures[monotonicDecreasingStack[monotonicDecreasingStack.length-1]]){
+            monotonicDecreasingStack.pop()
+        }
+
+        if(monotonicDecreasingStack.length > 0){
+            result[i] = monotonicDecreasingStack[monotonicDecreasingStack.length-1] - i
+        }
+        
+        monotonicDecreasingStack.push(i)
+    }
+    return result
+}
+
+
 var dailyTemperatures = function(temperatures) {
     const n = temperatures.length;
     const answer = new Array(n).fill(0);
