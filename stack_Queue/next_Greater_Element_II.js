cb72ca14f211027e7f491936dc8f925b851183f2
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-08-09 22:36:50 +0530

    next greater element without copying array

--- stack_Queue/next_Greater_Element_II.js
+++ stack_Queue/next_Greater_Element_II.js
@@ -1,6 +1,6 @@
 var nextGreaterElements = function (nums) {
     nums = [...nums, ...nums]
-    let stack = [nums[0]]
+    let stack = [nums[nums.length - 1]]
     let ans = new Array(nums.length).fill(-1)
     for (let i = nums.length - 2; i >= 0; i--) {
         while (nums[i] >= stack[stack.length - 1]) {
@@ -15,4 +15,24 @@ var nextGreaterElements = function (nums) {
 
     }
     return ans.slice(0, nums.length / 2)
+};
+
+
+
+var nextGreaterElements = function(nums) {
+   let stack=[nums[nums.length-1]]
+   let ans=new Array(nums.length).fill(-1)
+   for(let i=2*nums.length-2;i>=0;i--){
+    while(nums[i%nums.length]>=stack[stack.length-1]){
+        stack.pop()
+    }
+
+    if(stack.length>0){
+        ans[i%nums.length]=stack[stack.length-1]
+    }
+
+    stack.push(nums[i%nums.length])
+
+   } 
+ return ans
 };
