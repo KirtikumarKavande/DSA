c316ac96fd6b31e49de166ae69c29c79f706f3d8
Kirtikumar Kavande <121145138+KirtikumarKavande@users.noreply.github.com>
2025-08-13 05:27:14 +0000

    length of last word

---
+++ string/01_length_of_last_word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let array=s.split(" ")
+    for(let i=array.length-1;i>=0;i--){
+        if(array[i]!==''){
+            return array[i].length
+        }
+    }
+};
