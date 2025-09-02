ef465400a7e075d9cefa6ff201db643facd4194f
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-09-02 09:16:14 +0530

    largest odd number approch 2

--- string/08_Largest_Odd_Number_String.js
+++ string/08_Largest_Odd_Number_String.js
@@ -15,3 +15,15 @@ var largestOddNumber = function(num) {
     }
     return result
 };
+
+
+var largestOddNumber =function(num){
+    for (let i = num.length-1; i >=0; i--) {
+        
+        if(num[i] %2 !==0){
+            return num.substring(0,i+1)
+        }
+    }
+    return ""
+
+}
