b40bee6f489362d8c4f6931ccdec1bcd5f70d891
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-09-09 08:12:36 +0530

    isomorphic string approch 2

--- string/11_Isomorphic_Strings.js
+++ string/11_Isomorphic_Strings.js
@@ -13,3 +13,27 @@ var isIsomorphic = function (s, t) {
     }
     return true
 };
+
+
+// approch2 
+
+var isIsomorphic = function(s, t) {
+    const charIndexS = {};
+    const charIndexT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (!(s[i] in charIndexS)) {
+            charIndexS[s[i]] = i;
+        }
+
+        if (!(t[i] in charIndexT)) {
+            charIndexT[t[i]] = i;
+        }
+
+        if (charIndexS[s[i]] !== charIndexT[t[i]]) {
+            return false;
+        }
+    }
+
+    return true;    
+};
