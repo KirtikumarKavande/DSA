80502e3eb435bd11a7152c1de1c92bc4d7222fdc
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-09-19 09:20:33 +0530

    pre order traversal

---
+++ tree/01_pre-order_traversal.js
@@ -0,0 +1,14 @@
+var preorderTraversal = function(root) {
+    
+    const result=[]
+    function traversal(currNode){
+        if(!currNode) return 
+        result.push(currNode.val)
+        traversal(currNode.left)
+        traversal(currNode.right)
+    }
+
+    traversal(root)
+
+    return result;
+};
