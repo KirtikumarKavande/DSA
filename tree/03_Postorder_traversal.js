f8bd8907704a33e8d2cf8d17cff2b473436966b7
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-09-22 09:17:45 +0530

    post order traversal

---
+++ tree/03_Postorder_traversal.js
@@ -0,0 +1,14 @@
+var postorderTraversal  = function(root) {
+    const arr=[]
+    function traversal(currNode) {
+        if(!currNode) return
+            traversal(currNode.left)
+            traversal(currNode.right)
+            arr.push(currNode.val)
+
+    }
+    traversal(root)
+
+    return arr
+    
+};
