a1682753646575e93cda2ae63aa65f49527aad20
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-09-23 08:57:59 +0530

    pre order traversal iterative approach

---
+++ tree/04_pre-order_traversal_iterative.js
@@ -0,0 +1,19 @@
+var preorderTraversal = function(root) {
+const stack=[root]
+const result=[]
+if(!root) return []
+while(stack.length){
+    const top=stack.pop()
+    result.push(top.val)
+
+    if(top?.right?.val){
+    stack.push(top.right)
+    }
+    if(top?.left?.val){
+    stack.push(top.left)
+    }
+    
+}    
+return result
+
+};
