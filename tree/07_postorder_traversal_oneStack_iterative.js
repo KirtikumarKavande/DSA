965a9bddf5ae3706cb5c5b9f3bff6a594d65f43f
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-09-27 10:50:08 +0530

    postorder traversal  corrected code

--- tree/07_postorder_traversal_oneStack_iterative.js
+++ tree/07_postorder_traversal_oneStack_iterative.js
@@ -1,25 +1,23 @@
-
 var postorderTraversal = function (root) {
 
     if (!root) return []
     const result = []
     const stack = [root]
-    let lastPoppedNode = null
+    let lastVisited = ""
     let currentNode = root
-
     while (stack.length) {
         while (currentNode?.left) {
             currentNode = currentNode?.left
             stack.push(currentNode)
         }
         const topNode = stack[stack.length - 1]
-        if (topNode?.right && lastPoppedNode) {
-            currentNode = currentNode.right
+        if (topNode?.right && lastVisited!==topNode.right) {
+            currentNode = topNode.right
+            stack.push(currentNode)
         } else {
-            lastPoppedNode = stack.pop()
-            result.push(currentNode.val)
+            lastVisited = stack.pop()
+            result.push(lastVisited.val)
         }
     }
-
     return result
 };
