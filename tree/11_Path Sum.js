7020c52355f2384e6d79b5471c15d6800c12794a
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-10-04 11:33:45 +0530

    has sum approach 2

--- tree/11_Path Sum.js
+++ tree/11_Path Sum.js
@@ -27,3 +27,27 @@ var hasPathSum = function (root, targetSum) {
 
     return DFS(root, 0)
 };
+
+
+// approach 2
+
+
+var hasPathSum = function(root, targetSum) {
+    if(!root) return false;
+    let ans = false;
+
+    let traverse = (curr, currSum) => {
+        let newSum = currSum + curr.val;
+
+        if(!curr.left && !curr.right) {
+            if(newSum === targetSum){
+                ans = ans || true;
+            }
+        }
+        
+        curr.left && traverse(curr.left, newSum);
+        curr.right && traverse(curr.right, newSum);
+    } 
+    traverse(root, 0);
+    return ans;
+};
