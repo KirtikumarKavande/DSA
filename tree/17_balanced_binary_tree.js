a52a6ac0c02a91120ac451a41f8b01a121feda07
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-10-25 11:06:02 +0530

    balanced binary tree approach 2

--- tree/17_balanced_binary_tree.js
+++ tree/17_balanced_binary_tree.js
@@ -13,4 +13,19 @@ var isBalanced = function (root) {
 
     DFS(root)
     return result
-};   
+};
+
+
+// approach 2
+
+var isBalanced = function (root) {
+    function dfs(node) {
+        if (!node) return 0;
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        if (left === -1 || right === -1 || Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
+    return dfs(root) !== -1;
+};
