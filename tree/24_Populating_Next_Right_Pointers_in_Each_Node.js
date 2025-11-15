70df08704a8867a3230b9c1c1b80d62476493e59
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-11-15 11:28:06 +0530

    approach 2 without using q[0]

--- tree/24_Populating_Next_Right_Pointers_in_Each_Node.js
+++ tree/24_Populating_Next_Right_Pointers_in_Each_Node.js
@@ -23,3 +23,29 @@ var connect = function (root) {
 
 };
 
+//approach 2 without using q[0]
+var connect = function (root) {
+    if (!root) return root
+
+    const q = [root]
+    while (q.length) {
+        let prev = null
+        const len = q.length
+        for (let index = 0; index < len; index++) {
+            const node = q.shift()
+            if (node.left) {
+                q.push(node.left, node.right)
+            }
+            if (prev) {
+                prev.next = node
+            }
+            prev = node
+
+        }
+        prev.next = null
+    }
+
+    return root
+
+
+}
